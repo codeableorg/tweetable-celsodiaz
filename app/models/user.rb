e1c8f374ca67efc286e672df16f932dbd739eb10
celso <celso.acd.11@gmail.com>
2023-07-29 00:59:31 -0500

    fix login github

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
     #validates
     # validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
     validates :username, presence: true, uniqueness: true
-    validates :name, presence: true
     # validates :password, length: { minimum: 6 }, allow_nil: true
     #Associations
     has_one_attached :avatar
