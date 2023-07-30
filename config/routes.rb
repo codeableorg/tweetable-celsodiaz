92dfaa2aa7bb22eabe0a1d241b2283d6a92baa6e
celso <celso.acd.11@gmail.com>
2023-07-29 22:50:40 -0500

    add current

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   resources :likes
   resources :tweets
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "tweets#index"
